340411445cce5eafa7d56a95e9a1c1b36448649d
cece <sojeong.lee017@gmail.com>
2024-01-22 15:53:32 +0900

    fix: import error && chore: change pwa plugin

--- next.config.js
+++ next.config.js
@@ -5,23 +5,28 @@
  * visit https://www.npmjs.com/package/next-pwa or
  * https://ducanh-next-pwa.vercel.app/docs/next-pwa
  */
-const dev = process.env.NODE_ENV === "development";
-const withPWA = require("next-pwa")({
-    dest: "public",
-    disable: dev,
-    register: true,
-    skipWaiting: true,
-    sw: 'service-worker.js',
+const dev = process.env.NODE_ENV === "development"
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: dev,
+  register: true,
+  skipWaiting: true,
+  sw: "service-worker.js",
 
-    // runtimeCaching
-    runtimeCaching: [
-        // to customize runtime caching array,
-        // see https://github.com/DuCanhGH/next-pwa/blob/master/packages/next-pwa/src/cache.ts
-    ]
-});
+  // runtimeCaching
+  // to customize runtime caching array,
+  // see https://github.com/DuCanhGH/next-pwa/blob/master/packages/next-pwa/src/cache.ts
+
+  // extendDefaultRuntimeCaching: true,
+  // workboxOptions: {
+  //     runtimeCaching: [
+  //         // cache options
+  //     ]
+  // }
+})
 
 const nextConfig = withPWA({
-    // next.js config
-});
+  // next.js config
+})
 
-module.exports = nextConfig;
+module.exports = nextConfig
