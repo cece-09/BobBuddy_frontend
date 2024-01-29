952391cfbf4bca3501dc8b1cae051718c9399c46
kimfield98 <kimfield98@gmail.com>
2024-01-29 14:45:07 +0900

    remove: create-account components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client"
+import { RecoilRoot } from "recoil";
 import SigninPage from "./login/page";
 
 export default function MainPage() {
   return (
-    <>
+    <RecoilRoot>
       <SigninPage />
-    </>
+    </RecoilRoot>
   )
 }
