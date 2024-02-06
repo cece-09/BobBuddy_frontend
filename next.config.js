7076a1ff11e320c836f2085363af502822c5a167
cece <sojeong.lee017@gmail.com>
2024-02-06 16:43:40 +0900

    chore: next.js cors config

--- next.config.js
+++ next.config.js
@@ -27,6 +27,27 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig = withPWA({
   // next.js config
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ]
+  },
 })
 
 module.exports = nextConfig
